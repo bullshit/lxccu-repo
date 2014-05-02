5ae1cc96089a273bda4310fa933296fea32e8168
Oskar Holowaty <me@oskarholowaty.com>
2014-05-03 01:52:48 +0200

    rename

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 PACKAGENAME="lxccu-repo"
 MAINTAINER="Oskar Holowaty <me@oskarholowaty.com>"
 VENDOR="LXCU Team <team@lxccu.org>"
-VERSION="1.1-1"
+VERSION="1.2-1"
 URL="https://www.biglan.at/oskar/lxccu/"
 LICENSE="GPLv3"
 DESCRIPTION="Install lxccu repository"
@@ -21,7 +21,7 @@ fpm -f -s dir -t deb -a all \
 	--after-install "${ROOT}/debian/install_apt_key.sh" \
 	--post-uninstall "${ROOT}/debian/uninstall_apt_key.sh" \
 	-v "$VERSION" \
-	-p "${ROOT}/lxcu-repo_${VERSION}_all.deb" \
+	-p "${ROOT}/${PACKAGENAME}_${VERSION}_all.deb" \
 	--config-files "/etc/apt/" \
 	--deb-changelog "${ROOT}/debian/changelog" \
 	--deb-compression "xz" \
